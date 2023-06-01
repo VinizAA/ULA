78d342014516503c6a870b50458a199a2844269a
Vinicius Alvarez <110983068+VinizAA@users.noreply.github.com>
2023-06-01 16:22:24 -0300

    ula.vhd

--- ula.vhd
+++ ula.vhd
@@ -1,9 +1,10 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
-entity ula is 
-	port (a, b: in std_logic_vector(3 downto 0);
-			s: std_logic_vector (1 downto 0);
+entity ula is --entity da ula
+	port (a, b: in std_logic_vector(3 downto 0); --entradas: a/b são os números da conta
+			s: in std_logic_vector (1 downto 0); --entrada: s é a seleção da 
 			f: out std_logic_vector (3 downto 0); 
 			c, z, v, n: out std_logic);
 end ula;
@@ -14,14 +15,13 @@ architecture funcao of ula is
 	
 	component add4bits is 
 		port (a, b: in std_logic_vector(3 downto 0);
-			s: in std_logic;
-			f: out std_logic_vector (3 downto 0); 
-			cout: out std_logic);
+			   s: in std_logic;
+			   f: out std_logic_vector (3 downto 0); 
+			   cout: out std_logic);
 	end component;
 	
 begin
-	
-	res0: add4bits PORT MAP (a, b, s(0), f1, c);
+	res: add4bits PORT MAP (a, b, s(1), f1, c);
 	
 	f2 <= a and b;
 	f3 <= a or b;
@@ -31,12 +31,9 @@ begin
 		f3 when "11", --or
 		f1 when others; --soma/subtração
 		
-	v <= (a(3) and b(3) and not f4(3)) or (not a(3) and not b(3) and f4(3)) when s(1) = '0' else
-			'0' when s(1) = '1';
-	f <= f4;
-	z <= not(f4(0) or f4(1) or f4(2) or f4(3));
+	v <= (a(3) and b(3) and not f4(3)) or (not a(3) and not b(3) and f4(3)) when s(0) = '0' else --faz a conta do overflow
+			'0' when s(0) = '1'; --n faz a conta do overflow
+	z <= not(f4(0) or f4(1) or f4(2) or f4(3)); 
 	n <= f4(3);
-	
-	
-	
+	f <= f4;
 end funcao;
